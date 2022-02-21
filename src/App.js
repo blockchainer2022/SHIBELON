022f105a8cb6dbd5cac3b940f5386bc17d049e04
blockchainer2022 <91312709+blockchainer2022@users.noreply.github.com>
2022-02-21 16:58:39 +0530

    update

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ const App = () => {
     // you are connected to main net
     // Please connect to main net
 
-    if (chainId === 1) {
+    if (chainId === 4) {
       toast(`You are connected to main net`, {
         type: "success",
         position: toast.POSITION.BOTTOM_CENTER,
@@ -124,7 +124,7 @@ const App = () => {
 
   async function mint(mintCount) {
     if (contract) {
-      if (chainId === 1) {
+      if (chainId === 4) {
         const saleOpen = await contract.methods.saleOpen().call();
         if (saleOpen) {
           if (mintCount === 0) {
